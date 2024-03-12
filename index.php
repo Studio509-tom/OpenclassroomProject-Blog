ee2b9a29f1609646e282dc424917971007bc244a
Tom Delva <tom@studio509.fr>
2024-03-12 13:47:15 +0100

    issue #8 création des routes

--- index.php
+++ index.php
@@ -1,35 +1,53 @@
 <?php
-
 require_once("./vendor/autoload.php");
 
-$loader = new Twig_Loader_Filesystem(__DIR__ .'/templates');
-$twig = new Twig_Environment($loader , [
-    'cache'=> false,
-]);
+// $loader = new Twig_Loader_Filesystem(__DIR__ .'/templates');
+// $twig = new Twig_Environment($loader , [
+//     'cache'=> false,
+// ]);
 
 spl_autoload_register(function($fqcn){
     $path = str_replace(['Application','\\'],['src' , '/'] , $fqcn) . '.php';
     require_once($path);
 });
-
 use Application\Controllers\HomepageController;
-
+use Application\Controllers\UserController;
+// var_dump($_GET['action']);
+// var_dump($_GET);
 
 try{
     if (isset($_GET['action']) && $_GET['action'] !== ''){
-        if ($_GET['action'] === 'post') {
+        // var_dump($_GET);
+        if ($_GET['action'] === 'formRegister') {
+            $input = null;
+            if($_SERVER['REQUEST_METHOD'] ==='POST'){
+                $input = $_POST;
+            }
+            (new UserController())->register($input);
            
         }    
-        
-        else {
+        elseif ($_GET['action'] === 'register') {
+            (new UserController())->registerPage();
+        }
+        elseif ($_GET['action'] === 'login') {
+            (new UserController())->loginPage();
+        }
+        elseif ($_GET['action'] === 'formLogin'){
+            $input = null;
+            if($_SERVER['REQUEST_METHOD'] ==='POST'){
+                $input = $_POST;
+            }
+            (new UserController())->login($input);
+        }
+        else{
             throw new Exception("La page que vous recherchez n'existe pas.");
         }
     }
     else{
-        (new HomepageController())->homepage($twig);
+        (new HomepageController())->homepage();
     }
 }catch(Exception $e){
-    echo'tototo';
     $errorMessage = $e->getMessage();
-    echo $twig->render('error.html.twig', [ 'error'=>$errorMessage , 'title' => 'Erreur']); 
+    echo $errorMessage;
+    // echo $twig->render('error.html.twig', [ 'error'=>$errorMessage , 'title' => 'Erreur']); 
 }
