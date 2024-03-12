f2729367a666c68c9d59aac27d420a9beff7cbb2
Tom Delva <tom@studio509.fr>
2024-03-12 13:45:26 +0100

    issue #14 ParentController

---
+++ src/ParentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application;
+class ParentController
+{
+    public $twig;
+    public function __construct()
+    {
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../templates');
+        $this->twig = new \Twig_Environment($loader, [
+            'cache' => false,
+        ]);
+    }
+    
+}
