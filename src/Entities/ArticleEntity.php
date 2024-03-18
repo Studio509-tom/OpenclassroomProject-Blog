6a4ebb716a276fd613273660469560e7a32f6301
Tom Delva <tom@studio509.fr>
2024-03-18 14:09:47 +0100

    issue #5 ArticleEntity

---
+++ src/Entities/ArticleEntity.php
@@ -0,0 +1,15 @@
+<?php
+namespace Application\Entities;
+
+/**
+ * ArticleEntity
+ */
+class ArticleEntity {
+    public string $id ;
+    public string $title;
+    public string $chapo;
+    public string $content;
+    public $author;
+    public string $date;
+
+}
