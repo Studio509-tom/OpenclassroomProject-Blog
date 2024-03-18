23f4b02df76390c3d0d14c12d0f5f62cc6e9d79e
Tom Delva <tom@studio509.fr>
2024-03-18 14:14:27 +0100

    issue #6 UserModel

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -5,26 +5,52 @@ namespace Application\Model;
 use Application\Lib\DatabaseConnection;
 use Application\Entities\UserEntity;
 
-class UserModel {
-    public DatabaseConnection $connection;
-    public function __construct(){
+class UserModel
+{
+    public DatabaseConnection $connection;    
+    /**
+     * __construct
+     *
+     * @return void
+     */
+    public function __construct()
+    {
         $this->connection = new DatabaseConnection();
     }
-    public function addUser($name , $firstname , $email , $password_hash ){
+
+    /**
+     * Ajout d'un utilisateur en base de donnée
+     *
+     * @param  string $name
+     * @param  string $firstname
+     * @param  string $email
+     * @param  string $password_hash
+     * @return bool
+     */
+    public function addUser(string $name, string $firstname, string $email, string $password_hash)
+    {
         $statement = $this->connection->getConnection()->prepare(
             'INSERT INTO users( name, firstname , email , password) VALUES(?, ?, ?, ?)'
         );
-        $affectedLines = $statement->execute([$name, $firstname, $email , $password_hash]);
+        $affectedLines = $statement->execute([$name, $firstname, $email, $password_hash]);
         return ($affectedLines > 0);
     }
-    public function getUser($email){
+
+    /**
+     * Récupère l'utilisateur en base de donnée
+     *
+     * @param  string $email
+     * @return obj or null
+     */
+    public function getUser($email)
+    {
         $statement = $this->connection->getConnection()->prepare(
             'SELECT * FROM users WHERE email = ?'
         );
         $statement->execute([$email]);
         //Utilisation du fetch pour voir chacune des donné de notre tableau
         $row = $statement->fetch();
-        if($row === false) {
+        if ($row === false) {
             return null;
         }
         $user = new UserEntity;
@@ -35,9 +61,6 @@ class UserModel {
         $user->password = $row['password'];
 
         return $user;
-    }
-    public function login(){
-
     }
     
 }
